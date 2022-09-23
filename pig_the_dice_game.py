aa65a54032e8831e09b52599832685341538bf67
shc5529 <shc5529@naver.com>
2022-09-23 21:33:38 +0900

    refactor: roll or stop 입력받게 수정 및 턴 넘기면 점수 보존하게 수정

--- pig_the_dice_game.py
+++ pig_the_dice_game.py
@@ -9,11 +9,9 @@ player = 0
 score=0
 
 while max(safescore) < maxscore:
-	rolling = input(f"Player {player}: ({safescore[player]}, {score}) Rolling? (Y) ").strip().lower() in {'yes', 'y', ''}
+	rolling = input(f"Player {player}: ({safescore[player]}, {score}) Rolling? (roll) or (stop) ").strip().lower()
 
-	print((player, safescore[player], score))
-
-	if rolling:
+	if rolling == 'roll' or rolling == 'r':
 		rolled = randint(1, 6)
 		print(f'Rolled {rolled}')
 		if rolled == 1:
@@ -21,11 +19,10 @@ while max(safescore) < maxscore:
 			score, player = 0, (player + 1) % playercount
 		else:
 			score += rolled
-	else:
+	elif rolling == 'stop' or rolling == 's':
 		safescore[player] += score
 		if safescore[player] >= maxscore:
 			break
 		print(f'  Sticking with {safescore[player]}')
-		score, player = 0, (player + 1) % playercount
 		
 print(f'\nPlayer {player} wins with a score of {safescore[player]}')
